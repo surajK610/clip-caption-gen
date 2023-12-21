5331f0b5fd07b26b075bed994be5d42708cc3d7f
Suraj Anand <suraj_anand@brown.edu>
2023-12-21 16:35:18 -0500

    fixed key

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -2,7 +2,7 @@ unset XDG_RUNTIME_DIR
 
 #interact -n 20 -t 01:00:00 -m 10g
 module load python/3.7.4 cuda/11.7.1 gcc/10.2
-source ../interventions/interventions-venv/bin/activate
+source ~/interventions/interventions-venv/bin/activate
 ipnip=$(hostname -i)
 ipnport=8889
 echo "Paste the following command onto your local computer:"
